be4e79bc6f35524c1b90c12b72e112ab3590fb57
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-01-02 22:30:28 +0300

    Implemented Apollo & created a new ApolloClient instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import LaunchTimes from './components/LaunchTimes';
+import {ApolloClient, ApolloProvider, HttpLink} from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.spacex.land/graphql/',
+  }),
+})
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <ApolloProvider client={client}>
+        <LaunchTimes />
+      </ApolloProvider>
   );
 }
 
---
+++ src/components/LaunchTimes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LaunchTimes() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LaunchTimes;
